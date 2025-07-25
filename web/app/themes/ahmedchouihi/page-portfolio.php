c1e4be16a0f497cbc819e344bd4a5b077f60ed4a
Ahmed Chouihi <ahmedchouihi@gmail.com>
2025-07-25 16:10:17 +0100

    remove portfolio page (not used anymore)

--- web/app/themes/ahmedchouihi/page-portfolio.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template Name: Portfolio
- * 
- * This is a custom page template for the portfolio page.
- */
-
-// Include the main template
-get_template_part('resources/views/page-portfolio'); 
